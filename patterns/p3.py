eaad6de27cae294efb7a310b3f6c362013809eb7
darshan-bs-2005 <darshanbs189@gmail.com>
2026-06-28 15:49:08 +0530

    pattern3

---
+++ patterns/p3.py
@@ -0,0 +1,9 @@
+class pattern():
+    def pattern3(self,n):
+        for i in range(1,n+1):
+            for j in range(1,i+1):
+                print(j,end=" ")
+            print()
+
+p =pattern()
+p.pattern3(5)
