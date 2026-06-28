1597d042e6edcf326340af702831c55b78c552a6
darshan-bs-2005 <darshanbs189@gmail.com>
2026-06-28 19:36:12 +0530

    pattern7

---
+++ patterns/p7.py
@@ -0,0 +1,16 @@
+class pattern():
+    def pattern7(self,n):
+        for i in range(n): 
+            #for space
+            for j in range(n-i+1):
+                print(" ",end=" ")
+            #for star
+            for j in range(2*i+1):
+                print("*",end=" ")
+            # for space
+            for j  in range(n-i+1):
+                print(" ",end=" ")
+            print()
+
+p =pattern()
+p.pattern7(5)
---
+++ patterns/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+
+                print("*", end=" ")
+
+            print()
+
+p = pattern()
